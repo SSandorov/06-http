7cdd87a203dcbb82b1582ebba06819460a32949e
ssandorov <stefansandorovssb@gmail.com>
2022-06-05 19:40:00 +0200

    CRUD: Crear un usuario

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { init as initChistes } from "./js/chistes-page.js";
 import { init as initUsuarios } from "./js/usuarios-page.js";
 
 import { obtenerUsuarios } from "./js/http-provider.js";
+
+// Con el asterisco importamos todas las funciones exportadas en la página
 import * as CRUD from "./js/crud-provider.js";
 
 initChistes()
@@ -13,3 +15,7 @@ initUsuarios()
 obtenerUsuarios().then(console.log);
 
 CRUD.getUsuario(2).then(console.log);
+CRUD.crearUsuario({
+    name: 'Fernando',
+    job: 'Carpintero'
+}).then(console.log);
--- src/js/crud-provider.js
+++ src/js/crud-provider.js
@@ -1,4 +1,17 @@
 // Este servicio es muy parecido al http-provider
+
+/*
+Definición de CRUD
+
+- Create
+- Read
+- Update
+- Delete
+
+Son las peticiones básicas que se hacen para poder interactuar con una base de datos, 
+llamar un servicio, ...
+*/
+
 // al igual que el http-provider en primer lugar tenemos la url
 
 // En este caso empleamos la URL genérica, para después añadir las extensiones que 
@@ -6,10 +19,11 @@
 
 const urlCRUD = 'https://reqres.in/api/users';
 
-// Creamos la función que nos devolverá el usuario
+// Creamos la función que nos leerá el usuario
 const getUsuario = async(id) => {
 
     // especificamos la ruta del dominio que necesitamos
+    // en este caso es la petición GET -- SINGLE USER
     const resp = await fetch(`${urlCRUD}/${id}`);
 
     // añadimos una variable con el body de la petición y la retornamos
@@ -17,6 +31,30 @@ const getUsuario = async(id) => {
     return data;
 }
 
+// Creamos la función con la que crearemos el usuario
+const crearUsuario = async(usuario) => {
+
+    // especificamos la ruta del dominio que necesitamos
+    // en este caso es la petición POST -- CREATE
+    const resp = await fetch(urlCRUD, {
+        // Este objeto nos va a permitir configurar la petición POST
+        method: 'POST',
+        // el body es la data que yo quiero pedir
+        // debe ser un string, por lo que debemos convertir el JSON en string
+        body: JSON.stringify(usuario),
+        // los headers son data adicional sobre la petición que puede que el 
+        // back-end nos pida
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    // Comprobamos la respuesta
+    // console.log(await resp.json());
+    return await resp.json();
+
+}
+
 export {
-    getUsuario
+    getUsuario,
+    crearUsuario
 }
